85dcb4be660e54562d19dc8443c5ac139441c27b
Julio Daniel Arias Vitta <ariasvittad@gmail.com>
2026-08-12 22:09:33 -0500

    Mejorar sitemap SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,8 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://cronossl-comparador-energia-luz-gas.vercel.app";
+  const baseUrl =
+    "https://cronossl-comparador-energia-luz-gas.vercel.app";
 
   return [
     {
@@ -9,6 +10,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/con-factura`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/sin-factura`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
